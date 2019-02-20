86f5107c812a9f65a8d4fbb1de95e20940821ce5
Tipsy <642853815@qq.com>
2019-02-20 20:46:26 +0800

    把文件添加到本地Git

---
+++ day01/src/com/itheima/demo.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+    }
+
+}
